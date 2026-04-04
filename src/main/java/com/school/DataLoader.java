7d7d7c187087e489d0d3ee1f9df9d98f14170d9f
Biniyam Taye <biniyamtaye04@gmail.com>
2026-04-04 09:22:39 +0300

    feat: implement database data loader and dashboard view for school management system

--- src/main/java/com/school/DataLoader.java
+++ src/main/java/com/school/DataLoader.java
@@ -20,40 +20,14 @@ public class DataLoader implements CommandLineRunner {
         // Seed Students: id, first, last, email, grade, gpa, attendance
         studentRepository.save(new Student(null, "Alexander", "Sterling", "a.sterling@academy.edu", "Senior (12th)", 4.0, 99));
         studentRepository.save(new Student(null, "Elena", "Rodriguez", "e.rodriguez@academy.edu", "Senior (12th)", 3.9, 98));
-        studentRepository.save(new Student(null, "Marcus", "Chen", "m.chen@academy.edu", "Junior (11th)", 3.5, 94));
         studentRepository.save(new Student(null, "Sophia", "Laurent", "s.laurent@academy.edu", "Sophomore (10th)", 3.8, 96));
-        studentRepository.save(new Student(null, "Julian", "Vance", "j.vance@academy.edu", "Senior (12th)", 3.2, 91));
         studentRepository.save(new Student(null, "Aria", "Night", "a.night@academy.edu", "Freshman (9th)", 3.9, 99));
-        studentRepository.save(new Student(null, "Dominic", "Frost", "d.frost@academy.edu", "Junior (11th)", 3.4, 93));
         studentRepository.save(new Student(null, "Isabella", "Mikaelson", "i.mikaelson@academy.edu", "Sophomore (10th)", 4.0, 100));
-        studentRepository.save(new Student(null, "Ethan", "Hunt", "e.hunt@academy.edu", "Senior (12th)", 3.7, 95));
-        studentRepository.save(new Student(null, "Olivia", "Pope", "o.pope@academy.edu", "Junior (11th)", 3.9, 97));
-        studentRepository.save(new Student(null, "Lucas", "Scott", "l.scott@academy.edu", "Freshman (9th)", 2.8, 82));
-        studentRepository.save(new Student(null, "Maya", "Hart", "m.hart@academy.edu", "Sophomore (10th)", 3.6, 92));
-        studentRepository.save(new Student(null, "Noah", "Flynn", "n.flynn@academy.edu", "Senior (12th)", 2.4, 75));
-        studentRepository.save(new Student(null, "Chloe", "Decker", "c.decker@academy.edu", "Junior (11th)", 3.5, 96));
-        studentRepository.save(new Student(null, "Liam", "Dunbar", "l.dunbar@academy.edu", "Sophomore (10th)", 3.1, 85));
-        studentRepository.save(new Student(null, "Zoe", "Benson", "z.benson@academy.edu", "Freshman (9th)", 4.0, 100));
-        studentRepository.save(new Student(null, "Benjamin", "Franklin", "b.franklin@academy.edu", "Senior (12th)", 3.95, 98));
-        studentRepository.save(new Student(null, "Catherine", "Zeta", "c.zeta@academy.edu", "Junior (11th)", 3.82, 95));
-        studentRepository.save(new Student(null, "Daniel", "Craig", "d.craig@academy.edu", "Sophomore (10th)", 3.65, 92));
-        studentRepository.save(new Student(null, "Elizabeth", "Windsor", "e.windsor@academy.edu", "Freshman (9th)", 4.0, 100));
-        studentRepository.save(new Student(null, "Finn", "Hudson", "f.hudson@academy.edu", "Junior (11th)", 2.9, 88));
 
         // Seed Teachers: id, first, last, subject, department
         teacherRepository.save(new Teacher(null, "Dr. Sebastian", "Wilde", "Advanced Physics", "Science"));
-        teacherRepository.save(new Teacher(null, "Prof. Amara", "Vane", "Quantum Mechanics", "Science"));
         teacherRepository.save(new Teacher(null, "Ms. Evelyn", "Blackwood", "European History", "Humanities"));
-        teacherRepository.save(new Teacher(null, "Mr. Silas", "Crane", "Applied Mathematics", "STEM"));
-        teacherRepository.save(new Teacher(null, "Madam Clara", "Hale", "Classical Literature", "Arts"));
         teacherRepository.save(new Teacher(null, "Director Leo", "Drake", "Digital Innovation", "Technology"));
-        teacherRepository.save(new Teacher(null, "Dr. Victor", "Frank", "Biotechnology", "Science"));
         teacherRepository.save(new Teacher(null, "Prof. Diana", "Prince", "Ethics & Philosophy", "Humanities"));
-        teacherRepository.save(new Teacher(null, "Mr. Bruce", "Wayne", "Strategic Management", "Business"));
-        teacherRepository.save(new Teacher(null, "Ms. Selina", "Kyle", "Cyber Security", "Technology"));
-        teacherRepository.save(new Teacher(null, "Dr. Alfred", "Pennyworth", "Advanced Sociology", "Humanities"));
-        teacherRepository.save(new Teacher(null, "Prof. Charles", "Xavier", "Neurobiology", "Science"));
-        teacherRepository.save(new Teacher(null, "Ms. Natasha", "Romanoff", "Global Intelligence", "International Relations"));
-        teacherRepository.save(new Teacher(null, "Mr. Tony", "Stark", "Robotics & AI", "Technology"));
     }
 }
